1f67ad2ee3e1987e6dcee2ba5cc4b10360107278
desham25 <runwiththebull84@gmail.com>
2019-02-06 03:13:56 -0500

    adding more

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,55 +1,306 @@
-// Global Variables
-// ===============================================================================
-// Create an array of Words
-const word = ['Ace','airhead','ash people', 'bag your face', 'bite me', 'burn out','butt ugly','cool out','couch potato','crunchy'];
-// Choose word randomly
-Let randNum = Math.floor(Math.random() * word.length);
-Let choosenWord = word[randNum];
-let rightWord = [];
-let wrongWord = [];
-let underScore = [];
+window.onload = function () {
 
-// Dom Manipulation
-let docUnderScore = document.getElementsByClassName('underscore');
-let docRightGuess = document.getElementsByClassName('rightGuess');
-let docWrongGuess = document.getElementsByClassName('wrongGuess');
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint ;          // Word getHint
+    var word ;              // Selected word
+    var guess ;             // Geuss
+    var geusses = [ ];      // Stored geusses
+    var lives ;             // Lives
+    var counter ;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+  
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("scatagory");
+    var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+  
+  
+  
+    // create alphabet ul
+    var buttons = function () {
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+  
+      for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+      }
+    }
+      
+    
+    // Select Catagory
+    var selectCat = function () {
+      if (chosenCategory === categories[0]) {
+        catagoryName.innerHTML = "80s Catch Phrases";
+      } else if (chosenCategory === categories[1]) {
+        catagoryName.innerHTML = "80s Movies";
+      } else if (chosenCategory === categories[2]) {
+        catagoryName.innerHTML = "80s Cartoons";
+      }
+    }
+  
+    // Create geusses ul
+     result = function () {
+      wordHolder = document.getElementById('hold');
+      correct = document.createElement('ul');
+  
+      for (var i = 0; i < word.length; i++) {
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        if (word[i] === "-") {
+          guess.innerHTML = "-";
+          space = 1;
+        } else {
+          guess.innerHTML = "_";
+        }
+  
+        geusses.push(guess);
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+      }
+    }
+    
+    // Show lives
+     comments = function () {
+      showLives.innerHTML = "You have " + lives + " lives";
+      if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+      }
+      for (var i = 0; i < geusses.length; i++) {
+        if (counter + space === geusses.length) {
+          showLives.innerHTML = "You Win!";
+        }
+      }
+    }
+  
+        // Animate man
+    var animate = function () {
+      var drawMe = lives ;
+      drawArray[drawMe]();
+    }
+  
+    
+     // Hangman
+    canvas =  function(){
+  
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.strokeStyle = "#fff";
+      context.lineWidth = 2;
+    };
+    
+      head = function(){
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.arc(60, 25, 10, 0, Math.PI*2, true);
+        context.stroke();
+      }
+      
+    draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+      
+      context.moveTo($pathFromx, $pathFromy);
+      context.lineTo($pathTox, $pathToy);
+      context.stroke(); 
+  }
+  
+     frame1 = function() {
+       draw (0, 150, 150, 150);
+     };
+     
+     frame2 = function() {
+       draw (10, 0, 10, 600);
+     };
+    
+     frame3 = function() {
+       draw (0, 5, 70, 5);
+     };
+    
+     frame4 = function() {
+       draw (60, 5, 60, 15);
+     };
+    
+     torso = function() {
+       draw (60, 36, 60, 70);
+     };
+    
+     rightArm = function() {
+       draw (60, 46, 100, 50);
+     };
+    
+     leftArm = function() {
+       draw (60, 46, 20, 50);
+     };
+    
+     rightLeg = function() {
+       draw (60, 70, 100, 100);
+     };
+    
+     leftLeg = function() {
+       draw (60, 70, 20, 100);
+     };
+    
+    drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+  
+  
+    // OnClick Function
+     check = function () {
+      list.onclick = function () {
+        var geuss = (this.innerHTML);
+        this.setAttribute("class", "active");
+        this.onclick = null;
+        for (var i = 0; i < word.length; i++) {
+          if (word[i] === geuss) {
+            geusses[i].innerHTML = geuss;
+            counter += 1;
+          } 
+        }
+        var j = (word.indexOf(geuss));
+        if (j === -1) {
+          lives -= 1;
+          comments();
+          animate();
+        } else {
+          comments();
+        }
+      }
+    }
+    
+      
+    // Play
+    play = function () {
+      categories = [
+          ["airhead", "barf-me-out", "bodacious", "chill", "ditz", "dweeb", "eat-my-shorts"],
+          ["Top-Gun", "an-american-werewolf", "the-untouchables", "first-blood", "poltergeist"],
+          ["ducktales", "g-i-joe", "ghostbusters", "the-simpsons", "thunder-cats"]
+      ];
+  
+      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+      word = word.replace(/\s/g, "-");
+      console.log(word);
+      buttons();
+  
+      geusses = [ ];
+      lives = 10;
+      counter = 0;
+      space = 0;
+      result();
+      comments();
+      selectCat();
+      canvas();
+    }
+  
+    play();
+    
+    // Hint
+  
+      hint.onclick = function() {
+  
+        hints = [
+          ["A ditz", "Reaction to something offensive or disgusting", "Excellent or awesome Beautiful when describing a woman", "Relax; hang out. Also used to tell someone to relax by saying", "Not very smart. Same as airhead", "Geek or nerd", "Insult, used as a comeback"],
+          ["Navy fighter pilots", "Full Moon In London", "gangster flick", "highly trained former special forces soldier", "haunted house"],
+          ["Huey Dewey and Louie", "highly-trained elite strike force", "Who you going call", "longest running American animated program", "Lion-O and The Sword of Omens"]
+      ];
+  
+      var catagoryIndex = categories.indexOf(chosenCategory);
+      var hintIndex = chosenCategory.indexOf(word);
+      showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+    };
+  
+     // Reset
+  
+    document.getElementById('reset').onclick = function() {
+      correct.parentNode.removeChild(correct);
+      letters.parentNode.removeChild(letters);
+      showClue.innerHTML = "";
+      context.clearRect(0, 0, 400, 400);
+      play();
+    }
+  }
+  
+  
+  
 
-// Main
-// ===============================================================================
-console.log(choosenWord);
-// Create underscores based on the length words
-let generateUnderscore = () => {
-    for(let i = 0; i < choosenWord.length; i++){
-        underScore.push('_');
-    }
-    return underScore;
-}    
 
-// Get Users guess
-document.addEventListener('Keypress',(event) => {
-    let keycode = String.fromCharCode (event.keyCode);
-// if Users guess is right
-    if(choosenWord.indexOf(keyword) > -1) {
-        // add to right words array
-        rightWord.push(KeyWord);
-        // replace underscore with right letter
-        underScore[choosenWord.indexOf(keyword)] = keyword;
-        docUnderScore[0].innerHTML =  underScore.join(' ');
-        docRightGuess [0].innerHTML = rightWord;
-        // checks to see if user word matches guesses
-        if(underScore.join('') == choosenWord) {
-            alert('You Win');
-        }    
-    }
-    else{    
-       wrongWord.push(KeyWord);
-       docWrongGuess[0].innerHTML =wrongWord;
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// work #2
+// // Global Variables
+// // ===============================================================================
+// // Create an array of Words
+// const word = ['Ace', 'airhead', 'ash people', 'bag your face', 'bite me', 'burn out', 'butt ugly', 'cool out', 'couch potato', 'crunchy'];
+// // Choose word randomly 
+// var randNum = Math.floor(Math.random() * word.length);
+// var choosenWord = word[randNum];
+// let rightWord = [];
+// let wrongWord = [];
+// let underScore = [];
+
+// // Dom Manipulation
+// let docUnderScore = document.getElementsByClassName('underscore');
+// let docRightGuess = document.getElementsByClassName('rightGuess');
+// let docWrongGuess = document.getElementsByClassName('wrongGuess');
+
+// // Main
+// // ===============================================================================
+// console.log(choosenWord);
+// // Create underscores based on the length words
+// let generateUnderscore = () => {
+//     for(let i = 0; i < choosenWord.length; i++){
+//         underScore.push('_');
+//     }
+//     return underScore;
+// }    
+
+// // Get Users guess
+// document.addEventListener('Keypress',(event) => {
+//     let keycode = String.fromCharCode (event.keyCode);
+// // if Users guess is right
+//     if(choosenWord.indexOf(keyword) > -1) {
+//         // add to right words array
+//         rightWord.push(KeyWord);
+//         // replace underscore with right letter
+//         underScore[choosenWord.indexOf(keyword)] = keyword;
+//         docUnderScore[0].innerHTML =  underScore.join(' ');
+//         docRightGuess [0].innerHTML = rightWord;
+//         // checks to see if user word matches guesses
+//         if(underScore.join('') == choosenWord) {
+//             alert('You Win');
+//         }    
+//     }
+//     else{    
+//        wrongWord.push(KeyWord);
+//        docWrongGuess[0].innerHTML =wrongWord;
+//     }
+// });
  
 
-underScore[0].innerHTML = generateUnderscore().join(' ');
+// underScore[0].innerHTML = generateUnderscore().join(' ');
 
-// Check if guess is right
-// if right push to right array
-// if wrong push to wrong array
+// // Check if guess is right
+// // if right push to right array
+// // if wrong push to wrong array
